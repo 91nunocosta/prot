c970ced84fc6cbbda66e22e1c8789dd54b4c2132
91nunocosta <91nunocosta@gmail.com>
2023-03-12 22:50:00 +0000

    feat: add task for loading subgraphs into neo4j

--- tests/test_flows.py
+++ tests/test_flows.py
@@ -1,7 +1,32 @@
 """Test weave_challenge module."""
 from prefect.testing.utilities import prefect_test_harness
+from py2neo import Graph, Node, Relationship
 
-from weave_challenge.flows import ingest_unitprot_into_neo4j_flow
+from weave_challenge.flows import ingest_unitprot_into_neo4j_flow, load_into_neo4j
+
+
+def test_task_load_into_neo4j() -> None:
+    """Test loading a stream of subgraphs into neo4j."""
+    graph = Graph()
+    graph.delete_all()
+
+    alice = Node("Person", name="Alice")
+    bob = Node("Person", name="Bob")
+    carol = Node("Person", name="Carol")
+    knows = Relationship.type("KNOWS")
+    alice_bob = knows(alice, bob)
+    bob_alice = knows(bob, alice)
+    alice_carol = knows(alice, carol)
+    carol_alice = knows(carol, alice)
+    bob_carol = knows(bob, carol)
+    carol_bob = knows(carol, bob)
+    friends = alice_bob | bob_alice | alice_carol | carol_alice | bob_carol | carol_bob
+
+    load_into_neo4j.fn([friends])
+
+    assert len(graph.nodes) == 3
+
+    graph.delete_all()
 
 
 def test_ingest_unitprot_into_neo4j_flow() -> None:
--- weave_challenge/flows.py
+++ weave_challenge/flows.py
@@ -1,5 +1,20 @@
 """Defines flow ingesting UnitProt xml files data into neo4j."""
-from prefect import flow
+from typing import Iterable
+
+import py2neo
+from prefect import flow, task
+
+
+@task
+def load_into_neo4j(subgraphs: Iterable[py2neo.Subgraph]) -> None:
+    """Task loading a stream of subgraphs into neo4j.
+
+    Args:
+        subgraphs (Iterable[py2neo.Subgraph): stream of subgraphs.
+    """
+    graph = py2neo.Graph()
+    for subgraph in subgraphs:
+        graph.create(subgraph)
 
 
 @flow()
