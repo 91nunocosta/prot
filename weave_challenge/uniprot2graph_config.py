e24461ce4df0075f10a85e4cd01a6453f2fc1ca0
91nunocosta <91nunocosta@gmail.com>
2023-03-13 21:40:58 +0000

    feat(flow): config xml to graph translation

---
+++ weave_challenge/uniprot2graph_config.py
@@ -0,0 +1,16 @@
+"""Configures UniProt translation into properties graph."""
+from weave_challenge.xml_extract import XML2GraphConfig
+
+UNITPROT2GRAPTH_CONFIG = XML2GraphConfig(
+    node_labels={
+        "person": "Author",
+    },
+    relationship_labels={
+        "organism": "IN_ORGANISM",
+        "gene": "FROM_GENE",
+    },
+    collection_elements={
+        "authorList": "HAS_AUTHOR",
+    },
+    elements_for_merging_with_parents={"protein"},
+)
