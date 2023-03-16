6250cc20ed842ad5e9890fc819ab3a01d437439e
91nunocosta <91nunocosta@gmail.com>
2023-03-16 10:42:38 +0000

    feat(flow): coerce created property to date type

--- prot/uniprot2graph_config.py
+++ prot/uniprot2graph_config.py
@@ -1,9 +1,16 @@
 """Configures UniProt translation into properties graph."""
+import dateutil.parser
+
 from prot.xml_extract import XML2GraphConfig
 
 UNITPROT2GRAPTH_CONFIG = XML2GraphConfig(
     node_labels={
         "person": "Author",
+    },
+    property_types={
+        ("entry", "created"): (
+            lambda v: dateutil.parser.parse(v)  # pylint: disable=unnecessary-lambda
+        )
     },
     relationship_labels={
         "organism": "IN_ORGANISM",
